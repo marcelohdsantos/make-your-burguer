0fdda5fea96157df57b5558ba0f615c3f0297b4c
Marcelo <marcelodossantos732@yhaoo.com.br>
2022-02-01 21:55:13 -0400

    estilização da página home e banner principal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Banner />
+  <div class="main-container">
+    <h1>HOME</h1>
+    <p>formulário</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Banner from '../components/Banner.vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    Banner
   }
 }
+
 </script>
